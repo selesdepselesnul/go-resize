dfa5880cc9a60c775a34db28dca379620716ae73
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-23 14:17:28 +0700

    separate some function to different file

--- main.go
+++ main.go
@@ -5,83 +5,13 @@ import (
 	"image"
 	"path/filepath"
 	"github.com/urfave/cli"
-	"strconv"
 	"io"
 	"io/ioutil"
 	"os"
-	"archive/zip"
 	"log"
 	"strings"
 )
 
-func toUint(strInt string) uint {
-	parsedUint64, _ := strconv.ParseUint(strInt, 10, 64)
-	return uint(parsedUint64)
-}
-
-func unzip(src, dest string) error {
-    r, err := zip.OpenReader(src)
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := r.Close(); err != nil {
-            panic(err)
-        }
-    }()
-
-    os.MkdirAll(dest, 0755)
-
-    // Closure to address file descriptors issue with all the deferred .Close() methods
-    extractAndWriteFile := func(f *zip.File) error {
-        rc, err := f.Open()
-        if err != nil {
-            return err
-        }
-        defer func() {
-            if err := rc.Close(); err != nil {
-                panic(err)
-            }
-        }()
-
-        path := filepath.Join(dest, f.Name)
-
-        if f.FileInfo().IsDir() {
-            os.MkdirAll(path, f.Mode())
-        } else {
-            os.MkdirAll(filepath.Dir(path), f.Mode())
-            f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-            if err != nil {
-                return err
-            }
-            defer func() {
-                if err := f.Close(); err != nil {
-                    panic(err)
-                }
-            }()
-
-            _, err = io.Copy(f, rc)
-            if err != nil {
-                return err
-            }
-        }
-        return nil
-    }
-
-    for _, f := range r.File {
-        err := extractAndWriteFile(f)
-        if err != nil {
-            return err
-        }
-    }
-
-    return nil
-}
-
-func isDirectory(path string) (bool, error) {
-    fileInfo, err := os.Stat(path)
-    return fileInfo.IsDir(), err
-}
 
 func resizePng(file, output string, width, height uint) {
 	ImageResizer {
