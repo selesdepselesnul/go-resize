3f7de0dd51baf92661ef55ea6aa10cfde96082fe
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-23 09:16:00 +0700

    separate to different files

--- main.go
+++ main.go
@@ -1,52 +1,14 @@
-package  main
+package main
 import (
-	"github.com/nfnt/resize"
 	"image/png"
 	"image/jpeg"
 	"image"
-	"log"
-	"os"
 	"path/filepath"
 	"github.com/urfave/cli"
-	"fmt"
 	"strconv"
 	"io"
+	"os"
 )
-
-type ImageResizer struct {
-	file, output string
-	width, height uint
-	decoder func (io.Reader) (image.Image, error)
-	encoder func (io.Writer, image.Image) error
-}
-
-func (i ImageResizer) resize() {
-	ext := filepath.Ext(i.file)
-
-	file, err := os.Open(i.file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	img, err := i.decoder(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	file.Close()
-
-	m := resize.Resize(i.width, i.height, img, resize.Lanczos3)
-
-	out, err := os.Create(i.output + ext)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer out.Close()
-
-	i.encoder(out, m)
-
-	fmt.Println(i.output + ext)
-}
 
 func toUint(strInt string) uint {
 	parsedUint64, _ := strconv.ParseUint(strInt, 10, 64)
@@ -107,7 +69,7 @@ func main() {
 				func (w io.Writer, i image.Image) error {
 					return png.Encode(w, i)
 				},
-			}.resize()
+			}.Resize()
 		} else if ext == ".jpg" {
 			ImageResizer {
 				fileArg,
@@ -120,11 +82,8 @@ func main() {
 				func (w io.Writer, i image.Image) error {
 					return jpeg.Encode(w, i, nil)
 				},
-			}.resize()
+			}.Resize()
 		}
 	}
 }
-
-
-
 
---
+++ resizer.go
@@ -0,0 +1,45 @@
+package main
+import (
+	"github.com/nfnt/resize"
+	"log"
+	"fmt"
+	"path/filepath"
+	"os"
+	"io"
+	"image"
+)
+
+type ImageResizer struct {
+	file, output string
+	width, height uint
+	decoder func (io.Reader) (image.Image, error)
+	encoder func (io.Writer, image.Image) error
+}
+
+func (i ImageResizer) Resize() {
+	ext := filepath.Ext(i.file)
+
+	file, err := os.Open(i.file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	img, err := i.decoder(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+
+	m := resize.Resize(i.width, i.height, img, resize.Lanczos3)
+
+	out, err := os.Create(i.output + ext)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer out.Close()
+
+	i.encoder(out, m)
+
+	fmt.Println(i.output + ext)
+}
